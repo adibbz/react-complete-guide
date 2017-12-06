2ec3811d3de3fa813185ac4e18459ba82c9c3fc6
Andrew Dibb <andrewdibb@Andrews-MacBook-Pro.local>
2017-12-05 23:38:26 -0600

    Finish up module 1

--- src/App.js
+++ src/App.js
@@ -11,27 +11,57 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     //console.log('was clicked');
     // DON'T DO THIS this.state.persons[0] = "Max";
     this.setState({ 
       persons: [
-        {name: "Max", age: 26},
+        {name: newName, age: 26},
         {name: "Silas", age: 36},
         {name: "Kat", age: 54}
       ]
     })
   } 
 
+  nameChangedHanler = (event) => {
+    this.setState({ 
+      persons: [
+        {name: 'Maxxy', age: 26},
+        {name: event.target.value, age: 33},
+        {name: "Kat", age: 54}
+      ]
+    })  
+  }
+
   render() {
+    const styles = {
+      backgroundColor: '#fff',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a react app</h1>
         <p>This is really working!!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <button 
+          onClick={() => this.switchNameHandler('Dude')}
+          style={styles}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age} />
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Max!')}
+          changed={this.nameChangedHanler}>
+          My Hobbies: Racing
+        </Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age} />
       </div>
     );
   }
